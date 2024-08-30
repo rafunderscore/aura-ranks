07c172446cf97ab433bd10c544f5f13a3017ab99
Raphael S. <salajaraphael@gmail.com>
2024-08-31 00:52:44 +0100

    fix: evaluations styling

--- screens/profile/evalutions/evaluation.tsx
+++ screens/profile/evalutions/evaluation.tsx
@@ -4,7 +4,8 @@ import styles from "./evaluation.module.scss";
 import Island from "@/components/island";
 import Button from "@/components/button";
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
-import { compareAsc, format, intlFormat, intlFormatDistance } from "date-fns";
+import { intlFormatDistance } from "date-fns";
+import Image from "next/image";
 
 interface EvaluationProps extends React.HTMLAttributes<HTMLDivElement> {
   evaluation: UserEvaluation;
@@ -12,20 +13,6 @@ interface EvaluationProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export const Evaluation = ({ evaluation, end }: EvaluationProps) => {
-  const formatter = {
-    number: new Intl.NumberFormat("en-US", {
-      notation: "compact",
-      compactDisplay: "short",
-      maximumFractionDigits: 1,
-    }),
-    // use date-fns to format the date
-    date: {
-      format: (date: Date | number, type: string) => {
-        return format(date, type);
-      },
-    },
-  };
-
   function RelativeTime(date: Date) {
     const timeString = intlFormatDistance(new Date(date), new Date());
     return timeString;
@@ -35,20 +22,30 @@ export const Evaluation = ({ evaluation, end }: EvaluationProps) => {
     <div className={styles.evaluation}>
       <div className={styles.content}>
         <div className={styles.left}>
-          <div className={styles.avatar}></div>
+          <div className={styles.avatar}>
+            <Image
+              src={`https://avatar.vercel.sh/${evaluation.evaluator?.username}`}
+              alt={"Profile Picture"}
+              width={40}
+              height={40}
+            />
+          </div>
           <div className={styles.seperator} />
         </div>
         <div className={styles.right}>
           <div className={styles.heading}>
             <div className={styles.text}>
               <div className={styles.sentence}>
-                <p>@{evaluation.evaluator?.username}</p>
+                <p>{evaluation.evaluator?.username}</p>
+
                 <p>
                   evaluated and
                   {evaluation.sign === "positive" ? " added" : " removed"}
                 </p>
 
-                <p>AP {evaluation.aura_points_used}</p>
+                <div data-evaluation-type={evaluation.sign}>
+                  <p>AP {evaluation.aura_points_used}</p>
+                </div>
               </div>
               <p>·</p>
               <p>
